56bffab88b061bc1c04d3eb9898ff9208c1da70d
Sagar <sagar@gmail.com>
2024-12-17 13:05:30 +0530

    added

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,10 @@
 import boto3, cv2, time, numpy as np, matplotlib.pyplot as plt, random
 import base64, json
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
